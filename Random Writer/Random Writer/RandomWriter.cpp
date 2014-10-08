20f818db766753a8cd56812896297cdac7cff5b3
Luis Marquez <gunclub84@gmail.com>
2014-10-08 16:58:49 -0400

    added map

--- Random Writer/Random Writer/RandomWriter.cpp
+++ Random Writer/Random Writer/RandomWriter.cpp
@@ -11,14 +11,19 @@
 #include "console.h"
 #include <string>
 #include <fstream>
+#include <map>
+#include <vector>
 
 std::string promptForFile(std::ifstream & infile, std::string prompt);
-void processRandomWriter(std::ifstream & infile);
+void processRandomWriter(std::ifstream & infile,int order);
 
 int main() {
 	std::ifstream file;
+	int markovOrder;
 	promptForFile(file, "Enter the source text: ");
-	processRandomWriter(file);
+	std::cout<< "Enter Markov order [1-10]: ";
+	std::cin>>markovOrder;
+	processRandomWriter(file,markovOrder);
 
 
     return 0;
@@ -42,12 +47,48 @@ std::string promptForFile(std::ifstream & infile, std::string prompt){
 
 }
 
-void processRandomWriter(std::ifstream & infile){
+void processRandomWriter(std::ifstream & infile,int order){
 
 	char ch;
+	std::map<std::string,std::vector<char> > seedMap;
+	std::map<std::string,std::vector<char> >::iterator p;
+	//std::vector<char> nextChar;
+	std::string str;
+	//std::istream_iterator<std::string> iit (std::ifstream infile);
+	//iit=infile.begin();
+
 	while(infile.get(ch))//read file one char at a time until eof is reached.
 	{
-		std::cout<<ch<<std::endl;
+		
+		for(int i=0;i<order;i++){
+			str +=ch;
+			infile.get(ch);
+			if(!infile.good())
+				break;
+		}
+		//if(map.count(str))
+		p=seedMap.find(str);
+		if(p!=seedMap.end()){
+			std::vector<char> vecChar = p->second;
+		//	seedMap[str]=p->second;//.push_back(infile.peek());
+			//seedMap[str]=p->second.push_back(infile.peek()); //"first" correspnds to key, "second" to value, need to use -> to dereference the p iterator
+		}
+		else{
+			char c= infile.peek();
+			std::vector<char> nextChar;
+			nextChar.push_back(c);
+			seedMap.insert(std::pair<std::string,std::vector<char> >(str,nextChar));//nextChar.push_back(infile.peek()));
+			//seedMap[str]=std::vector<char> vecChar2 ('d');
+		}
+
+		//map.insert(str,nextChar);
+		
+	//	nextChar.push_back(infile.peek(ch));
+		//map.insert(str,nextChar);
+		//std::cout<<ch<<std::endl;
+	//	std::cout<<infile.peek()<<std::endl;
+	//	std::cout<<*iit<<std::endl;
+	//	seedMap.insert(ch,ch+infile.g);
 	}
 	//std::fstream fin("file", fstream::in);
 //	while (fin >> noskipws >> ch) {
