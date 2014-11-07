bdd523303f2e5879cf9b895e3781784ba2fb6788
Luis Marquez <gunclub84@gmail.com>
2014-11-07 17:04:31 -0500

    added GridPoint class comments

--- Boggle/src/GridPoint.cpp
+++ Boggle/src/GridPoint.cpp
@@ -1,62 +1,66 @@
+/********************************************************************
+* File: GridPoint.cpp
+*
+* Created by : LuisMbedder
+*
+* Description : this file implements the GridPoint.h interface.
+********************************************************************/
+
 #include "GridPoint.h"
 #include <string>
 #include "strlib.h"
 
+
+//Constructor, creates a GridPoint object with origin coordinates
 GridPoint::GridPoint(void)
 {
 	x=0;
 	y=0;
 }
 
+//Constructor creates a GridPoint object with the specified x,y coordinates
 GridPoint::GridPoint(int xc,int yc){
 	x=xc;
 	y=yc;
 }
 
+//Getter, returns the x-coordiante of the GridPoint
 int GridPoint::getX(){
 	return x;
 }
 
+//Getter, returns the y-coordiante of the GridPoint
 int GridPoint::getY(){
 	return y;
 }
 
+//returns a string representation of the GridPoint in the form "(x,y)"
 std::string GridPoint::toString(){
 	return "(" + integerToString(x) + "," + integerToString(y) + ")"; 
 }
 
+//overloads the insertion operator
+std::ostream & operator<<(std::ostream & os, GridPoint pt){
+	return os << pt.toString();
+		
+}
+
+//free function, overloads the == operator
 bool operator==(GridPoint p1,GridPoint p2){
 	return p1.x == p2.x && p1.y == p2.y;
 }
 
+//free function, overloads the != operator
 bool operator!=(GridPoint p1, GridPoint p2){
 	return !(p1==p2);
 }
-/*
-bool operator<(const GridPoint& other)const{//overload < operator to use set class
-	if(x!=other.getX())
-		return x<other.getX();
-	return y<other.getY();
-	//return ((p2.getX()<p1.getX())&&(p2.getY()<p1.getY()));
-		
-}
-*/
 
+//free function, overloads the < operator
 bool operator<(GridPoint p1,GridPoint p2){//overload < operator to use set class
 if(p1.getX() != p2.getX())
 return p1.getX() < p2.getX();
 return p1.getY() < p2.getY();
 	//return ((p2.getX()<p1.getX())&&(p2.getY()<p1.getY()));
-		
-}
-
-/*
-bool operator>(GridPoint p1, GridPoint p2){//overlaod > operator to use set class
-	return ((p2.getX()>p1.getX())&&(p2.getY()>p1.getY()));
-}*/
-
-std::ostream & operator<<(std::ostream & os, GridPoint pt){
-	return os << pt.toString();
 		
 }
 
--- Boggle/src/GridPoint.h
+++ Boggle/src/GridPoint.h
@@ -1,4 +1,11 @@
-
+/********************************************************************
+* File: GridPoint.h
+*
+* Created by : LuisMbedder
+*
+* Description : this interface exports the GridPoint class which
+*				represents a point on a 2-D integer grid. 
+********************************************************************/
 #ifndef _GridPoint_h
 #define _GridPoint_h
 
@@ -8,16 +15,40 @@
 class GridPoint
 {
 public:
-	GridPoint(void);
-	GridPoint(int xc,int yc);
 
-	int getX();
-	int getY();
+	/*
+	 * Constructor: GridPoint
+	 * Usage: GridPoint origin;
+	 *		  GridPoint pt(x,y);
+	 * ---------------------------
+	 * creates a GridPoint Object. The default constructor sets the
+	 * coordinates to 0,0; the second sets the coordiantes to x,y
+	*/
+		GridPoint(void);
+		GridPoint(int xc,int yc);
 
-	std::string toString();
+	/*
+	 * Methods: getX,getY
+	 * Usage: int x = pt.getX();
+	 *		  int x = pt.getY();
+	 * ---------------------------
+	 * returns the x and y coordinates of the grid point. 
+	*/
+		int getX();
+		int getY();
 
+	/*
+	 * Method: toString
+	 * Usage: string str=pt.toString();
+	 * ---------------------------
+	 * returns a string representation of the GridPoint in the form "(x,y)"
+	*/
+		std::string toString();
+
+	//friend declaration
 	friend bool operator==(GridPoint p1,GridPoint p2);
 
+	//instance variables
 	int x;
 	int y;
 
@@ -25,13 +56,36 @@ public:
 	~GridPoint(void);
 };
 
-std::ostream & operator<<(std::ostream & os,GridPoint pt);
+	/*
+	 * Operator: <<
+	 * Usage: cout<<pt
+	 * ---------------------------
+	 * overloads the << operator to display GridPoint values
+	*/
+		std::ostream & operator<<(std::ostream & os,GridPoint pt);
 
-bool operator==(GridPoint p1, GridPoint p2);
+	/*
+	 * Operator: ==
+	 * Usage: p1==p2
+	 * ---------------------------
+	 * to support equality testing for GridPoints
+	*/
+		bool operator==(GridPoint p1, GridPoint p2);
 
-bool operator!=(GridPoint p1,GridPoint p2);
+	/*
+	 * Operator: !=
+	 * Usage: p1!=p2
+	 * ---------------------------
+	 * adds support for the != operator
+	*/
+		bool operator!=(GridPoint p1,GridPoint p2);
 
-//bool operator>(GridPoint p1, GridPoint p2);
-bool operator<(GridPoint p1, GridPoint p2);
+	/*
+	 * Operator: <
+	 * Usage: p1<p2
+	 * ---------------------------
+	 * to support comparison testing. 
+	*/
+		bool operator<(GridPoint p1, GridPoint p2);
 
 #endif
